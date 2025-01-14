fe0c3019521314faa4ef165a24622d9540d05aff
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-01-14 12:21:45 +0000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
+
+    let rdt;
+    $effect(() => {
+        rdt = RadixDappToolkit({
+            dAppDefinitionAddress:
+                "account_rdx16xdanhhgzzyen33q3fq3ljhekjh0ezh2gnu6z0gcrtsn9u29s2rwu7",
+            networkId: 1,
+            applicationName: "example-dapp",
+        });
+    });
+</script>
+
+<radix-connect-button></radix-connect-button>
+
+<style>
+    radix-connect-button {
+        position: fixed;
+        top: 10x;
+        right: 10px;
+        --radix-connect-button-border-radius: 5px;
+        --radix-connect-button-width: 36px;
+    }
+    @media (min-width: 640px) {
+        radix-connect-button {
+            --radix-connect-button-width: 140px;
+        }
+    }
+</style>
